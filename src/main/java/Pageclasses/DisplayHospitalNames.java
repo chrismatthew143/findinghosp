a11d81513944a11c4721e97dd19f8c020d4d52ee
CHRIS MATTHEW <CHRIS MATTHEW@LAPTOP-7ALDEHE7>
2021-05-17 19:41:50 +0530

    first commit

--- src/main/java/Pageclasses/DisplayHospitalNames.java
+++ src/main/java/Pageclasses/DisplayHospitalNames.java
@@ -1,4 +1,4 @@
-package Tasks;
+package Pageclasses;
 
 import java.io.IOException;
 import java.util.List;
--- src/main/java/Pageclasses/FillDetailsAndCaptureMessage.java
+++ src/main/java/Pageclasses/FillDetailsAndCaptureMessage.java
@@ -1,4 +1,4 @@
-package Tasks;
+package Pageclasses;
 
 import java.io.File;
 import java.io.IOException;
